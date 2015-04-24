828263240d92c63443f0e1d37dc6deff72d91db5
rodavoce <up201404178@fe.up.pt>
2015-04-24 13:01:29 +0100

    update Board
    Board read function

--- BatalhaNavalPROJ2/BatalhaNavalPROJ2/BOARD.cpp
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/BOARD.cpp
@@ -1,11 +1,33 @@
 #include "BOARD.h"
 #include <string>
 #include <fstream>
+#include "SHIP.h"
 
 Board::Board(const string &filename)
 {
 	
-	ifstream ReadConfig(filename)
+	ifstream ReadConfig(filename);
 
+		if (ReadConfig.is_open())
+		{	 
+			//leitura das dimensoes do tabuleiro
+			string absor; // usada para absorver "Tabuleiro: "
+			char XX;
+			getline(ReadConfig, absor, ' ');
+			ReadConfig >> numLines >> XX >> numColumns;
+			ReadConfig.ignore(1000, '\n');
+			//leitura dos navios
+			if (!ReadConfig.eof)
+			{
+				unsigned int ship_size, ship_colour;
+				PositionChar ship_position;
+				char ship_symbol,ship_orientation;
+				ReadConfig >> ship_symbol >> ship_size >> ship_position.lin >> ship_position.col >> ship_orientation >> ship_colour;
+
+				Ship::Ship()
+
+				
+			}
+		}
 	
 }
