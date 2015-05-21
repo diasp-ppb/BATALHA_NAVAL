13e5828157106706400479f1d16b6066fd343709
rodavoce <up201404178@fe.up.pt>
2015-05-22 00:29:10 +0100

    Scoreboard
    base do scoreboard

---
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.cpp
@@ -0,0 +1,24 @@
+#include "SCOREBOARD.h"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+scoreboard::scoreboard()
+{
+	ifstream Readscore("ScoreBoard.txt");
+
+
+
+}
+void scoreboard::update_scoreboard()
+{
+}
+bool scoreboard::top_scores()
+{
+	return true;
+}
+void scoreboard::show_scores()
+{
+
+}
