fbc1f4d7cc83950a74a0d7d3a72ebfd3db68f4fb
rodavoce <up201404178@fe.up.pt>
2015-05-22 12:57:04 +0100

    scoreboard
    sss

--- BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.cpp
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
+
 scoreboard::scoreboard()
 {
-	ifstream Readscore("ScoreBoard.txt");
+
+ifstream Readscore("ScoreBoard.txt");
 
 	while (!Readscore.eof())
 	{
@@ -18,12 +21,30 @@ scoreboard::scoreboard()
 		names.push_back(nn);
 		scores.push_back(sc);
 	}
-
-
-
 }
-void scoreboard::update_scoreboard()
+void scoreboard::update_scoreboard(string &playername, size_t &score)
 {
+	PL newhighscore; newhighscore.name = playername;  newhighscore.score = score;
+	size_t i;
+	for ( i = 0; i < scores.size(); i++)
+	{
+		if (scores[i].score < score)
+			break;
+	}
+	
+	vector<PL> newscores;
+	
+	for (size_t j = 0; j < i; j++)
+	{
+		newscores.push_back(scores[j]);
+	}
+
+	newscores.push_back(newhighscore);
+
+	for (i = i;i < scores.size(); i++)
+	{
+		newscores.push_back(scores[i]);
+	}
 }
 bool scoreboard::top_scores(size_t &sc) const
 {
@@ -35,6 +56,13 @@ bool scoreboard::top_scores(size_t &sc) const
 	return false;
 }
 void scoreboard::show_scores()
-{
-
+{   
+	cout << "=================================" << endl
+		 << "             SCOREBOARD            " << endl
+	<< endl;
+		 for (size_t i = 0; i < scores.size(); i++)
+		 {
+			 cout <<  setw(10) << left << scores[i].name << setw(12) << right << "--------->" << setw(10) << right << scores[i].score << endl;
+		 }
+		 cout << endl << "=================================" << endl;
 }
--- BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.h
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.h
@@ -1,14 +1,21 @@
 
 #include <vector>
 #include <string>
+ 
+using namespace std;
+struct PL
+{
+	string name;
+	size_t score;
+};
+
 class scoreboard
 {
 public:
 	scoreboard();
-	void update_scoreboard();
+	void update_scoreboard(string &playername,size_t &score);
 	bool top_scores(size_t & sc) const;
 	void show_scores();
 private:
-	std::vector <string> names;
-	std::vector <size_t> scores;
+	vector < PL > scores;
 };
