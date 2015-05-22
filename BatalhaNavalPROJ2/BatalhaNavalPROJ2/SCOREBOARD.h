29edb3980e3bde5d6aafa77cf8617bbef2444193
rodavoce <up201404178@fe.up.pt>
2015-05-22 15:38:30 +0100

    Revert "scoreboard"
    This reverts commit fbc1f4d7cc83950a74a0d7d3a72ebfd3db68f4fb.

--- BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.h
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/SCOREBOARD.h
@@ -1,21 +1,14 @@
 
 #include <vector>
 #include <string>
- 
-using namespace std;
-struct PL
-{
-	string name;
-	size_t score;
-};
-
 class scoreboard
 {
 public:
 	scoreboard();
-	void update_scoreboard(string &playername,size_t &score);
+	void update_scoreboard();
 	bool top_scores(size_t & sc) const;
 	void show_scores();
 private:
-	vector < PL > scores;
+	std::vector <string> names;
+	std::vector <size_t> scores;
 };
