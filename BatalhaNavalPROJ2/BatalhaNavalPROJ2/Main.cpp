7eabd0a708a8a831acb21087cab3e2278a9b9ba1
rodavoce <up201404178@fe.up.pt>
2015-05-23 15:37:15 +0100

    LE ISTO
    Adiciona no Board, o calculo da area ocupada pelos navios
    ====================================
    int Board::get_ships_size() const
    {
    int a = 0;
    for (size_t i = 0; i < ships.size(); i++)
    {
    a = a + ships[i].get_ship_size();
    }
    return a;
    }
    ===================================
    
    main,
    esta construido a estrutura base.

--- BatalhaNavalPROJ2/BatalhaNavalPROJ2/Main.cpp
+++ BatalhaNavalPROJ2/BatalhaNavalPROJ2/Main.cpp
@@ -4,42 +4,39 @@
 #include "BOARD.h"
 #include "SHIP.h"
 #include "SCOREBOARD.h"
-//#include "MENU.h"
+#include <cstdlib>
 #include <string>
 #include <iostream>
 #include <Windows.h>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
+#include <ctime>
 
 
 using namespace std;
 
 void print_boards(Player &UM, Player &DOIS);
 void player_turn(Player &UM, Player &DOIS);
-//int menu();
+void menu();
+int select_menu();
+void playgame();
+void player_win(Player &UM, Player &DOIS, double &endtime);
+void shoW_scoreboard();
 
 void main()
 {
 	srand((unsigned int)time(NULL)); // nao alterar
 
-	
-	scoreboard tabel = scoreboard();
-	tabel.show_scores();
-	tabel.update_scoreboard("joao", 12);
-	tabel.update_scoreboard("joao", 12);
-	tabel.update_scoreboard("joao", 12);
-	tabel.update_scoreboard("joao", 100);
-	tabel.show_scores();
-	system("pause");
+	 menu();
+}
 
-
-
-
+void playgame()
+{
 
 	Player UM = Player(1);
 	Player DOIS = Player(2);
-
+	const clock_t begin_time = clock();
 	do
 	{
 		system("cls");
@@ -48,171 +45,90 @@ void main()
 
 		if (DOIS.Player_DEAD() == true) break;
 
-		// gotoxy(0, 4 + UM.get_board().getLines());
+		
 		system("cls");
 		print_boards(UM, DOIS);
 		player_turn(DOIS, UM); // turno PL 2
 
-		//gotoxy(0, 0);
-		//UM.showBoard();
-		//gotoxy(0, 8 + UM.get_board().getLines());
+	
 
 		if (UM.Player_DEAD() == true) break;
 		print_boards(UM, DOIS);
 	} while (UM.Player_DEAD() != true || DOIS.Player_DEAD() != true);
+	/// TEMPO FINAL //
+	double end_time = (clock() - begin_time) / CLOCKS_PER_SEC; // resultado em segundos;
+	
 
-	///////////////////////////////////////////////////////////////////////
-	/*
-	Beep(330, 100); Sleep(100);
-	Beep(330, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(330, 100); Sleep(300);
-	Beep(392, 100); Sleep(700);
-	Beep(196, 100); Sleep(700);
-	Beep(262, 300); Sleep(300);
-	Beep(196, 300); Sleep(300);
-	Beep(164, 300); Sleep(300);
-	Beep(220, 300); Sleep(100);
-	Beep(246, 100); Sleep(300);
-	Beep(233, 200);
-	Beep(220, 100); Sleep(300);
-	Beep(196, 100); Sleep(150);
-	Beep(330, 100); Sleep(150);
-	Beep(392, 100); Sleep(150);
-	Beep(440, 100); Sleep(300);
-	Beep(349, 100); Sleep(100);
-	Beep(392, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(100);
-	Beep(247, 100); Sleep(500);
-	Beep(262, 300); Sleep(300);
-	Beep(196, 300); Sleep(300);
-	Beep(164, 300); Sleep(300);
-	Beep(220, 300); Sleep(100);
-	Beep(246, 100); Sleep(300);
-	Beep(233, 200);
-	Beep(220, 100); Sleep(300);
-	Beep(196, 100); Sleep(150);
-	Beep(330, 100); Sleep(150);
-	Beep(392, 100); Sleep(150);
-	Beep(440, 100); Sleep(300);
-	Beep(349, 100); Sleep(100);
-	Beep(392, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(100);
-	Beep(247, 100); Sleep(900);
-	Beep(392, 100); Sleep(100);
-	Beep(370, 100); Sleep(100);
-	Beep(349, 100); Sleep(100);
-	Beep(311, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(207, 100); Sleep(100);
-	Beep(220, 100); Sleep(100);
-	Beep(262, 100); Sleep(300);
-	Beep(220, 100); Sleep(100);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(500);
-	Beep(392, 100); Sleep(100);
-	Beep(370, 100); Sleep(100);
-	Beep(349, 100); Sleep(100);
-	Beep(311, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(523, 100); Sleep(300);
-	Beep(523, 100); Sleep(100);
-	Beep(523, 100); Sleep(1100);
-	Beep(392, 100); Sleep(100);
-	Beep(370, 100); Sleep(100);
-	Beep(349, 100); Sleep(100);
-	Beep(311, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(207, 100); Sleep(100);
-	Beep(220, 100); Sleep(100);
-	Beep(262, 100); Sleep(300);
-	Beep(220, 100); Sleep(100);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(500);
-	Beep(311, 300); Sleep(300);
-	Beep(296, 300); Sleep(300);
-	Beep(262, 300); Sleep(1300);
-	Beep(262, 100); Sleep(100);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(300);
-	Beep(330, 200); Sleep(50);
-	Beep(262, 200); Sleep(50);
-	Beep(220, 200); Sleep(50);
-	Beep(196, 100); Sleep(700);
-	Beep(262, 100); Sleep(100);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(100);
-	Beep(330, 100); Sleep(700);
-	Beep(440, 100); Sleep(300);
-	Beep(392, 100); Sleep(500);
-	Beep(262, 100); Sleep(100);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(294, 100); Sleep(300);
-	Beep(330, 200); Sleep(50);
-	Beep(262, 200); Sleep(50);
-	Beep(220, 200); Sleep(50);
-	Beep(196, 100); Sleep(700);
-	/*Intro*/
-	/*
-	Beep(330, 100); Sleep(100);
-	Beep(330, 100); Sleep(300);
-	Beep(330, 100); Sleep(300);
-	Beep(262, 100); Sleep(100);
-	Beep(330, 100); Sleep(300);
-	Beep(392, 100); Sleep(700);
-	Beep(196, 100); Sleep(700);
-	Beep(196, 100); Sleep(125);
-	Beep(262, 100); Sleep(125);
-	Beep(330, 100); Sleep(125);
-	Beep(392, 100); Sleep(125);
-	Beep(523, 100); Sleep(125);
-	Beep(660, 100); Sleep(125);
-	Beep(784, 100); Sleep(575);
-	Beep(660, 100); Sleep(575);
-	Beep(207, 100); Sleep(125);
-	Beep(262, 100); Sleep(125);
-	Beep(311, 100); Sleep(125);
-	Beep(415, 100); Sleep(125);
-	Beep(523, 100); Sleep(125);
-	Beep(622, 100); Sleep(125);
-	Beep(830, 100); Sleep(575);
-	Beep(622, 100); Sleep(575);
-	Beep(233, 100); Sleep(125);
-	Beep(294, 100); Sleep(125);
-	Beep(349, 100); Sleep(125);
-	Beep(466, 100); Sleep(125);
-	Beep(587, 100); Sleep(125);
-	Beep(698, 100); Sleep(125);
-	Beep(932, 100); Sleep(575);
-	Beep(932, 100); Sleep(125);
-	Beep(932, 100); Sleep(125);
-	Beep(932, 100); Sleep(125);
-	Beep(1046, 675);*/
-	/////////////////////////////////////////////////////////////////////// 
+	if (UM.Player_DEAD())
+	{
+		cout << "PARABENS!" << DOIS.get_player_name() << "ganhas-te!!!" << endl;
+		player_win(UM, DOIS, end_time);
+		
+	}
+		
+	else if (DOIS.Player_DEAD())
+	{
+		cout << "PARABENS!" << UM.get_player_name() << "ganhas-te!!!" << endl;
+		player_win(DOIS, UM, end_time);
+	}
+		
 
 }
-/*
-int menu()
+
+void menu()
 {
 	cout << "    Batalha Naval" << endl;
 	cout << "1 - Jogar " << endl;
 	cout << "2 - Pontuacao" << endl;
 	cout << "3 - Sair " << endl;
+	cout << endl << endl;
+	
+	switch (select_menu())
+	{
+	case 1:
+		playgame();
+		break;
+	case 2:
+		shoW_scoreboard();
+		break;
+	case 3:
+		exit(0);
+		break;
+	}
+	system("pause");
+	
+
+
 }
-*/
 
+int select_menu()
+{
+	int a;
+	bool invalid = false;
+	do
+	{
+	invalid = false;
+	cout << "Qual e a opcao? ";
 
+	cin >> a;
+	if (cin.fail())
+	{
+		cin.clear();
+	}
+	else if ( a < 1 || a > 3)
+	{	invalid = true;
+		cout << "Introduza uma das opcoes.... Por favor!" << endl;
+		
+	}
+	
+	
+	
+
+	} while (cin.fail() || invalid );
+	
+	return a;
+
+}
 void print_boards(Player &UM, Player &DOIS)
 {
 	gotoxy(0, 0);
@@ -231,4 +147,29 @@ void player_turn(Player &UM, Player &DOIS)
 {
 	cout << "Jogador: " << UM.get_player_name() << endl;
 	DOIS.attackBoard(UM.getBomb());
+}
+void player_win(Player &UM, Player &DOIS,double &endtime)
+{
+	cout << "PARABENS!" << DOIS.get_player_name() << "ganhas-te!!!" << endl;
+	size_t Area = DOIS.get_board().getColumns() * DOIS.get_board().getLines();
+	size_t score = endtime * (DOIS.get_board().get_ships_size() / Area);
+	scoreboard TAB = scoreboard();
+	if (TAB.top_scores(score))
+	{
+		cout << "ESTAS NO TOP 10!!" << endl;
+		system("pause");
+		TAB.update_scoreboard(UM.get_player_name(), score);
+		system("cls");
+	}
+	TAB.show_scores();
+	system("pause");
+	menu();
+}
+void shoW_scoreboard()
+{
+	scoreboard TAB = scoreboard();
+	TAB.show_scores();
+	system("pause");
+	system("cls");
+	menu();
 }
